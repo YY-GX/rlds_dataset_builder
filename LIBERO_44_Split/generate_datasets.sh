b389ed59907e98c83f1a50506d95883112283634
yy-gx <yyang9923@gmail.com>
2024-11-21 22:02:23 -0500

    add libero_44 dataset

--- LIBERO_44_Split/generate_datasets.sh
+++ LIBERO_44_Split/generate_datasets.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-PARENT_DIR="$(pwd)"
+PARENT_DIR="."
 TASKS=(
     "KITCHEN_SCENE10_close_the_top_drawer_of_the_cabinet"
     "KITCHEN_SCENE10_put_the_black_bowl_in_the_top_drawer_of_the_cabinet"
